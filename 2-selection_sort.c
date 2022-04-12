f482345a41af436ffaf5c185560921e39ed7be0f
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-04-12 12:47:13 -0700

    selection-sort-02

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - check the code
+ * @array: array
+ * @size: size of array
+ * Return: none
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, jMin;
+	int x;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		/* find the min element in the unsorted a[i .. aLength-1] */
+
+		/* assume the min is the first element */
+		jMin = i;
+		/* test against elements after i to find the smallest */
+		for (j = i + 1; j < size; j++)
+		{
+		/* if this element is less, then it is the new minimum */
+			if (array[j] < array[jMin])
+		/* found new minimum; remember its index */
+				jMin = j;
+		}
+
+		if (jMin != i)
+		{
+			x = array[i];
+			array[i] = array[jMin];
+			array[jMin] = x;
+		}
+	}
+}
+
